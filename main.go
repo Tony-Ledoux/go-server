eec6085fe91a072c3974483def955000fd87920d
Tony Ledoux <tony.ledoux@student.thomasmore.be>
2025-12-07 12:41:23 +0100

    end lesson 4

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	hits := cfg.fileServerHits.Load()
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -11,6 +37,7 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	apiCfg := &apiConfig{}
 	log.Println("Starting server on :8080")
 
 	mux.HandleFunc("/healthz", func(resp http.ResponseWriter, req *http.Request) {
@@ -19,8 +46,13 @@ func main() {
 		resp.Write([]byte("OK"))
 	})
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
+
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
